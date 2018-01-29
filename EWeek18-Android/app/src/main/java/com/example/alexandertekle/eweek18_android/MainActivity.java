538dfe66663bc3f24ff556523bf899a5da8979bd
Alexander Tekle <tekle.alexander@gmail.com>
2018-01-29 02:34:01 -0600

    Working on ListView

--- EWeek18-Android/app/src/main/java/com/example/alexandertekle/eweek18_android/MainActivity.java
+++ EWeek18-Android/app/src/main/java/com/example/alexandertekle/eweek18_android/MainActivity.java
@@ -4,17 +4,29 @@ package com.example.alexandertekle.eweek18_android;
         import android.support.design.widget.TabLayout;
         import android.support.v7.app.AppCompatActivity;
         import android.support.v7.widget.Toolbar;
+        import android.widget.ArrayAdapter;
+        import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     TabLayout tabLayout;
-
+    ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initInstancesDrawer();
+
+        mListView = (ListView)findViewById(R.id.events_listview);
+        String items[] = new String[10];
+
+        for (int i=0;i<items.length;i++)
+        {
+            items[i]="Event " + i;
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.activity_main)
     }
 
     private void initInstancesDrawer() {
